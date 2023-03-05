234234b0f2a5b2bfb16c04b10abeadaa5f8b3935
JohnnyIsNotAvailable <kabevzhanbek@gmail.com>
2023-03-06 00:12:58 +0600

    some changes are required

--- re7.py
+++ re7.py
@@ -7,4 +7,4 @@ print(re.sub(pattern, f, txt))
 
 # "(?P<g1>[a-z])(?P<g2>[A-Z])+"
 
-#mObject.group("g1")+ "_" + mObject.group("g2").lower()  
+#mObject.group("g1")+ "_" + mObject.group("g2").lower()
