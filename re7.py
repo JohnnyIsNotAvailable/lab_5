b2e5bafc21303d2a1de07a389febb544e9873d13
JohnnyIsNotAvailable <kabevzhanbek@gmail.com>
2023-03-06 18:02:54 +0600

    k

--- re7.py
+++ re7.py
@@ -1,10 +1,6 @@
 import re
-def f(snake_string):
-     return snake_string.title().replace("_", "")
+def f(mObject):
+    return mObject.group("g1") + mObject.group("g2").upper()
 txt = input()
-pattern = "(?P<g1>[a-z])_(?P<g2>[A-Z])+"
-print(re.sub(pattern, f, txt))
-
-# "(?P<g1>[a-z])(?P<g2>[A-Z])+"
-
-#mObject.group("g1")+ "_" + mObject.group("g2").lower()
+pattern = "(?P<g1>[a-z])_{1}(?P<g2>[a-z]){1}"
+print(re.sub(pattern, f, txt)) 
